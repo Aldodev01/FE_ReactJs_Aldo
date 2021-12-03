0831f39aec2437a751927cda7f1b55a67bcbe2cb
Aldodev01 <aldodevox@gmail.com>
2021-12-03 01:02:15 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,29 @@ import Carder from "./components/Carder";
 import { useQuery } from "react-query";
 import { data_get } from "./api/API";
 import { useEffect, useState } from "react";
-import { Space, Spin } from "antd";
+import { Spin } from "antd";
 
 function App() {
+  //! Statement untuk menyimpan data dari Query
   const [stateData, setStateData] = useState();
 
+  // !Data Dari API yang disimpan ke Query
   const {
     isLoading: data_loading,
     isError: data_error,
     data: data_data,
   } = useQuery("data_get", data_get);
 
+  // * Jika Ada data dari Query maka print data ke console
   if (data_data) {
     console.log(stateData);
   }
 
+  // ! Proses Data Disimpan ke State
   useEffect(() => {
     setStateData(data_data?.data);
   }, [data_data]);
+
   return (
     <main className="App">
       {stateData ? (
